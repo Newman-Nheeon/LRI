1fcd744f87c6b72a25011691fafb7b5b2ee17574
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2023-03-11 23:04:22 +0100

    Restructured the folder and install the necessary packages.

---
+++ frontend/src/pages/Onboarding/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ForgotPassword.scss";
+
+const ForgotPassword = () => {
+  return (
+    <section className="forget">
+      <article className="forget__container">
+        <div className="forget__container-contents"></div>
+      </article>
+    </section>
+  );
+};
+
+export default ForgotPassword;
