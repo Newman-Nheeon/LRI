0b3d2961a0ffc9080115bc55fc33b8ea3218dc95
Newman-Nheeon <newmanenyoojo@gmail.com>
2023-03-17 22:45:00 +0100

    Backend Updated

--- backend/Config/allowedOrigins.js
+++ backend/Config/allowedOrigins.js
@@ -1,5 +1,4 @@
 const allowedOrigins = [
-    'yourdomain.com',
     'http://localhost:3000/',
     'http://localhost:5000/'
 ];
--- backend/Config/corsOptions.js
+++ backend/Config/corsOptions.js
@@ -1,12 +1,15 @@
 const allowedOrigins = require('../Config/allowedOrigins') 
+
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
+
         }else {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    Credentials: true,
     optionsSuccessStatus: 200
 }
 
